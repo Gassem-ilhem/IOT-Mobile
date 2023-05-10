08a2e3c33ddd29071dab010a2a61daedfa408f7f
Gassem-ilhem <gassem.ilhem@gmail.com>
2023-05-11 01:57:06 +0200

    final

---
+++ app/src/main/java/com/example/iotfinal/PushNotificationService.java
@@ -0,0 +1,11 @@
+package com.example.iotfinal;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class PushNotificationService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+    }
+}
